8f88ed9697ce31582b794645fcc158be416e1905
Keightyan <keightyan@gmail.com>
2023-03-12 12:14:37 +0900

    230312

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Folder;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    public function index() {
-        return 'Hello World!';
+    public function index(int $id) {
+
+        $folders = Folder::all();
+        $current_folder_id = $id;
+
+        return view('tasks/index', compact('folders', 'current_folder_id'));
     }
 }
